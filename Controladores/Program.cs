19b88e87ace75897c294d8d93c480dc5682bee3b
csi22-irodhan <ivanrodriiguez04@gmail.com>
2023-10-31 11:50:39 +0100

    31102023

---
+++ Controladores/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Ejercicio5Bucles 
+{
+    /// <summary>
+    /// Clase principal de la aplicacion
+    /// 30102023 -> irm
+    /// </summary>
+    class Program 
+    { 
+        /// <summary>
+        /// Metodo principal de la aplicacion
+        /// 30102023 -> irm
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args) 
+        {
+            //Atributos
+            string respuestaintroducida="";
+            //Creamos los objetos
+        
+            //Creamos el bucle do..while
+            do 
+            { 
+                
+            } while (respuestaIntroducida=="s");
+        }
+    }
+}
